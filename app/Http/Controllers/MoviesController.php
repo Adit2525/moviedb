afedfc3c5d20ff4fb3fe04e97d23f7a49597eb81
Adit2525 <160556720+Adit2525@users.noreply.github.com>
2025-05-19 16:13:46 +0700

    homepage view

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movies;
 use Illuminate\Http\Request;
 
 class MoviesController extends Controller
 {
-    //
+    public function homepage(){
+        $movies = Movies::latest()->paginate(6);
+        // dd($movies);
+        return view('homepage',compact('movies'));
+    }
 }
