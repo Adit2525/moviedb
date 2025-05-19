0a8e17d4aa5a49590c6b66cddc5bc4e57b583ace
Adit2525 <160556720+Adit2525@users.noreply.github.com>
2025-05-19 14:35:28 +0700

    seeding dan factory

--- database/factories/MoviesFactory.php
+++ database/factories/MoviesFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Str;
+use Stringable;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movies>
@@ -14,10 +17,21 @@ class MoviesFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+   public function definition(): array
+{
+    $title = fake()->sentence(rand(3, 6));
+    $slug = Str::slug($title);
+
+    return [
+        'title' => $title,
+        'slug' => $slug,
+        'synopsis' => fake()->paragraph(rand(5, 10)),
+        'category_id' => category::inRandomOrder()->first(),
+        'year' => fake()->year(),
+        'actors' => fake()->name() . ', ' . fake()->name() . ', ' . fake()->name(),
+        'cover_image' => 'https://picsum.photos/seed/' . Str::random(10) . '/480/640',
+        'created_at' => now(),
+        'updated_at' => now(),
+    ];
+}
 }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,38 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categories')-> insert([
+
+        [
+            'category_name' => 'Actions',
+            'description' => 'Film dengan adegan aksi yang sangat bagus',
+            'created_at'=> now(),
+            'updated_at'=> now(),
+        ],
+        [
+            'category_name' => 'Comedy',
+            'description' => 'Film dengan adegan comedy yang jenaka',
+            'created_at'=> now(),
+            'updated_at'=> now(),
+        ],
+        [
+            'category_name' => 'Drama',
+            'description' => 'Film dengan pengembangan karakter dan konflik',
+            'created_at'=> now(),
+            'updated_at'=> now(),
+        ],
+        [
+            'category_name' => 'Sci-fi',
+            'description' => 'Film tentang technology',
+            'created_at'=> now(),
+            'updated_at'=> now(),
+        ],
+        [
+            'category_name' => 'Romance',
+            'description' => 'Film dengan adegan adegan percintaan yang sangat romantis',
+            'created_at'=> now(),
+            'updated_at'=> now(),
+        ],
+    ]);
     }
 }
