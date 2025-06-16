76d4c45ef9e403d9d6432e73a33ff69ba54136a1
Adit2525 <160556720+Adit2525@users.noreply.github.com>
2025-06-16 16:11:20 +0700

    tugas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Movies;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Movie;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,13 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // // User::factory(10)->create();
+         User::factory(10)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(CategorySeeder::class);
-        Movies::factory(50)->create();
+        // $this->call(CategorySeeder::class);
+        // Movie::factory(50)->create();
+
     }
 }
